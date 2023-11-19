51362d723672922c3ea06ca6635711d0a5e98870
geon <gunho1107@naver.com>
2023-11-19 11:33:25 +0900

    plot progressing

---
+++ view/plot.py
@@ -0,0 +1,34 @@
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure
+
+
+class Plot:
+    def __init__(self, logger):
+        self._fig = self._init_fig()
+        self._log = logger
+
+    def add_data(self, name: str, data_source: ColumnDataSource, color: str):
+        self._fig.line(
+            x="datetime", y="value",
+            line_color=color, line_width=2,
+            source=data_source, legend_label=name
+        )
+        self._fig.circle(
+            x="datetime", y="value",
+            line_color=color, size=2,
+            source=data_source, legend_label=name
+        )
+
+    def get(self):
+        return self._fig
+
+    @staticmethod
+    def _init_fig() -> figure:
+        return figure(
+            title="Soft hall tactile sensor",
+            x_axis_type="datetime",
+            x_axis_label="Date",
+            y_axis_label="Value",
+            sizing_mode="stretch_width",
+            tools="pan,box_zoom,reset,save,wheel_zoom"
+        )
--- view/root.py
+++ view/root.py
@@ -1,19 +1,23 @@
 import random
 
-from bokeh.plotting import figure, curdoc
+from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource
 from bokeh.palettes import Category20_20 as Palette
 
 from model.board import Board
+from view.plot import Plot
 
 
 class Root:
-    def __init__(self, doc):
+    def __init__(self, doc, logger=None):
         self._data_sources = {}
-        self._lines = {}
-        self._figure = self._get_figure()
         self._doc = doc
-        doc.add_root(self._figure)
+        if logger is not None:
+            self._log = logger
+        else:
+            self._log = lambda x: print(x)
+        self._plot = Plot(self._log)
+        doc.add_root(self._plot.get())
 
     def add_board(self, board: Board):
         for source_name, source in board.get_data_sources().items():
@@ -31,17 +35,4 @@ class Root:
                 line_color=color, line_width=2,
                 source=data_source, legend_label=name
             )
-            self._lines[name] = line
         self._doc.add_next_tick_callback(modify_doc)
-
-    @staticmethod
-    def _get_figure():
-        fig = figure(
-            title="Data from Multiple Producers (Every Second)",
-            x_axis_type="datetime",
-            sizing_mode="stretch_width",
-            tools="pan,box_zoom,reset,save,wheel_zoom"
-        )
-        fig.xaxis.axis_label = "Date"
-        fig.yaxis.axis_label = "Value"
-        return fig
